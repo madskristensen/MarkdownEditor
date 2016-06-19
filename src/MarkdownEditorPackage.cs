af3a84bd7c107b3319f05527a4d1e9d507ad618d
Mads Kristensen <post@madskristensen.net>
2016-06-19 01:04:54 -0700

    Autoload package

--- src/MarkdownEditorPackage.cs
+++ src/MarkdownEditorPackage.cs
@@ -8,6 +8,7 @@ namespace MarkdownEditor
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
     [ProvideOptionPage(typeof(Options), "Text Editor\\Markdown", "General", 101, 111, true, new[] { "markdown", "md" }, ProvidesLocalizedCategoryName = false)]
     [Guid(_packageGuidString)]
     public sealed class MarkdownEditorPackage : Package
