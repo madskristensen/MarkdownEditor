0472ce33d2799dd912451a4d4c3e4e570cdc1755
Mads Kristensen <post@madskristensen.net>
2016-06-22 09:15:26 -0700

    Added EditorFactory

--- src/MarkdownEditorPackage.cs
+++ src/MarkdownEditorPackage.cs
@@ -8,6 +8,7 @@ namespace MarkdownEditor
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    [ProvideEditorExtension(typeof(EditorFactory), ".md", 99)]
     [ProvideOptionPage(typeof(Options), "Text Editor\\Markdown", "General", 101, 111, true, new[] { "markdown", "md" }, ProvidesLocalizedCategoryName = false)]
     [Guid(PackageGuids.guidPackageString)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
